7eee5f03d7519b1732b419bcd3078f8b71cdfdbd
ElenaAl <61346270+ElenaAl@users.noreply.github.com>
2021-02-21 21:18:19 +0300

    Add files via upload
    corrected  code for tanos sort

--- src/tf/qa/automation/Main.java
+++ src/tf/qa/automation/Main.java
@@ -1,11 +1,11 @@
 package tf.qa.automation;
-import java.util.Arrays;
+
 
 public class Main {
 
     public static void main(String[] args) {
 
-        int[] x = new int[]{8,1,4,2,20,9,-5};
+        int[] x = new int[]{8, 1, 4, 2, 20, 9, -5,500,-150};
 
         System.out.println("До сортировки");
         for (int i = 0; i < x.length; i++) {
@@ -13,69 +13,64 @@ public class Main {
             System.out.print(x[i] + " ");
         }
 
-        tanosSort(x,x[0], x.length);
+        int low = 0;
+        int high = x.length - 1;
+
+        tanosSort(x, low, high);
 
         System.out.println("\nПосле");
         for (int i = 0; i < x.length; i++) {
 
             System.out.print(x[i] + " ");
-        }
+       }
 
     }
 
-    public static void tanosSort (int[] array, int low, int high) {
-        low = 0;
-        high = array.length-1;
+    public static void tanosSort(int[] array,int low,int high ) {
 
-        if(array.length==0)
-            return;
+        if (array.length == 0)
+            return;//завершить выполнение если длина массива равна 0
 
-        if (low >=high)
-            return;
+        if (low >= high)
+            return;//завершить выполнение если нечего делить
 
         int sum = 0;
-        int count=0;
-
+        int count = 0;
         int i = low, j = high;
-        // for (i=0; i< array.length-1; i++) {
-        boolean isSorted = false;
+        int i2=0;
+        for (i2 = low; i2 < high; i2++) {
+            sum += array[i2];
+            count++;
+        }
+        int average = sum / count;
 
 
-            for (i = 0; i < array.length - 1; i++) {
-                sum += array[i];
+        while (i <= j) {
+            while (array[i] < average) {
                 i++;
-                count++;
-            }
-            int average = sum / count;
 
-            // while(i<=j)        {
-
-            for (i = 0; i < array.length - 1; i++) {
-                while (array[i] < average) {
-                    i++;
-                }
-
-
-                while (array[j] > average) {
-                    j--;
-                }
-
-                if (i <= j) {
-
-                    int buffer = array[i];
-                    array[i] = array[j];
-                    array[j] = buffer;
-                    i++;
-                    j--;
-                }
             }
 
-            if (low < j)
-                tanosSort(array, low, j);
+            while (array[j] > average) {
+                j--;
+            }
 
-            if (high > i)
-                tanosSort(array, i, high);
+            if (i <= j) {
+
+                int buffer = array[i];
+                array[i] = array[j];
+                array[j] = buffer;
+                i++;
+                j--;
+            }
+        }
+
+        if (low < j)
+            tanosSort(array,low, j);
+
+        if (high > i)
+            tanosSort(array, i, high);
 
         }
 
-}
+    }
