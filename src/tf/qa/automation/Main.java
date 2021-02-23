66b174db0392cff4f4033cd9cd0bd035f5bd5652
ElenaAl <alb.helen@gmail.com>
2021-02-23 23:00:53 +0300

    добавлен метод проверки телефона, и изменен алгоритм проверки

--- src/tf/qa/automation/Main.java
+++ src/tf/qa/automation/Main.java
@@ -1,81 +1,105 @@
 package tf.qa.automation;
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
 
+
     public static void main(String[] args) {
 
-        int[] x = new int[]{8,1,4,2,20,9,-5};
-
-        System.out.println("До сортировки");
-        for (int i = 0; i < x.length; i++) {
-
-            System.out.print(x[i] + " ");
-        }
-
-        tanosSort(x,x[0], x.length);
-
-        System.out.println("\nПосле");
-        for (int i = 0; i < x.length; i++) {
-
-            System.out.print(x[i] + " ");
-        }
-
+        System.out.println("Введите номер телефона:");
+        Scanner sc = new Scanner(System.in);
+        String phone = sc.nextLine();
+        sc.close();
+        phoneCheck(phone);
     }
 
-    public static void tanosSort (int[] array, int low, int high) {
-        low = 0;
-        high = array.length-1;
+    public static void phoneCheck(String phone){
+        String spacebr = "В номере есть пробелы и/или скобки";
+        String wrongN="Введен некорректный номер";
+        String replace = "Замена +7 на 8";
+        String overfloat="Количество символов больше 11";
+        String small="Количество символов менее 11";
+        String check="Список изменений и сработавших проверок на некорректность: ";
+        int x=phone.length();
 
-        if(array.length==0)
-            return;
+        phone = phone.replace("+7", "8");
 
-        if (low >=high)
-            return;
-
-        int sum = 0;
-        int count=0;
-
-        int i = low, j = high;
-        // for (i=0; i< array.length-1; i++) {
-        boolean isSorted = false;
-
-
-            for (i = 0; i < array.length - 1; i++) {
-                sum += array[i];
-                i++;
-                count++;
-            }
-            int average = sum / count;
-
-            // while(i<=j)        {
-
-            for (i = 0; i < array.length - 1; i++) {
-                while (array[i] < average) {
-                    i++;
+        if(phone.length()!=x) { // если замена сработала
+            int y=phone.length();
+            phone = phone.replace("(", "");
+            phone = phone.replace(")", "");
+            phone = phone.replace(" ", "");
+            if(phone.length()!=y) { // если скобки были
+                if(phone.length()==11) {
+                    System.out.println(phone);
+                    System.out.println(check +replace +";"+ spacebr); //"Замена +7 на 8";"В номере есть пробелы и/или скобки";
                 }
-
-
-                while (array[j] > average) {
-                    j--;
-                }
-
-                if (i <= j) {
-
-                    int buffer = array[i];
-                    array[i] = array[j];
-                    array[j] = buffer;
-                    i++;
-                    j--;
+                else {
+                    System.out.println(wrongN); //"Введен некорректный номер";
+                    if(phone.length()>11) {
+                        System.out.println(overfloat); //>11;
+                    }
+                    else {
+                        System.out.println(small); // <11
+                    }
                 }
             }
+            else { // если скобок не было
+                if(phone.length()==11) {
+                    System.out.println(phone);
+                    System.out.println(check + replace); //"Замена +7 на 8";
+                }
+                else {
+                    System.out.println(wrongN); //"Введен некорректный номер";
+                    if(phone.length()>11) {
+                        System.out.println(overfloat); //>11;
+                    }
+                    else {
+                        System.out.println(small); // <11
+                    }
+                }
+            }
+        }
+        else { // если замена +7 на 8 не сработала
+            phone = phone.replace("(", "");
+            phone = phone.replace(")", "");
+            phone = phone.replace(" ", "");
 
-            if (low < j)
-                tanosSort(array, low, j);
+            if(phone.length()!=x) { // если сработала замена скобок
 
-            if (high > i)
-                tanosSort(array, i, high);
-
+                if(phone.length()==11) {// если были скобки и не было +7 и номер введен верно
+                    System.out.println(phone);
+                    System.out.println(check + spacebr);// "В номере есть пробелы и/или скобки"
+                }
+                else {
+                    System.out.println(wrongN); //"Введен некорректный номер";
+                    if(phone.length()>11) {
+                        System.out.println(overfloat); //>11;
+                    }
+                    else {
+                        System.out.println(small); // <11
+                    }
+                }
+            }
+            else{ // если ни одна замена не сработала
+                if(phone.length()==11) {
+                    System.out.println(phone); // изначально корректный номер
+                    System.out.println(check + "Не было изменений");
+                }
+                else{
+                    if(phone.length()<11) {
+                        System.out.println(wrongN); //"Введен некорректный номер";
+                        System.out.println(small); //<11
+                    }
+                    else{
+                        System.out.println(wrongN); //"Введен некорректный номер";
+                        System.out.println(overfloat); //>11;
+                    }
+                }
+            }
         }
 
+
+    }
 }
+
